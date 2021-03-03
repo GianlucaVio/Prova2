eff9b42be98f4035bc343cf2f28e47be74234589
ITSVioG <ITSVioG@itscor207240.itsacademy.corvallisholding.it>
2021-03-03 10:10:08 +0100

    prima prova

---
+++ Prova2/src/com/itskennedy/corsoCloudDeveloper/prova2.java
@@ -0,0 +1,13 @@
+package com.itskennedy.corsoCloudDeveloper;
+
+public class prova2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ciao ciao ciao");
+		
+		System.out.println("ciao ciao");
+	}
+
+}
